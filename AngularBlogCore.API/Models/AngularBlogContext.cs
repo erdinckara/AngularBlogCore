0ac45b5d2408a6926cbf0987c7b5fe5e458b412f
Erdinc Kara <erdinc.kara@primegov.com>
2020-03-27 22:54:39 +0000

    Add project files.

---
+++ AngularBlogCore.API/Models/AngularBlogContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularBlogCore.API.Models
+{
+    public partial class AngularBlogContext : DbContext
+    {
+        public AngularBlogContext()
+        {
+        }
+
+        public AngularBlogContext(DbContextOptions<AngularBlogContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Article> Article { get; set; }
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Comment> Comment { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>(entity =>
+            {
+                entity.Property(e => e.ContentMain).IsRequired();
+
+                entity.Property(e => e.ContentSummary)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Picture).HasMaxLength(300);
+
+                entity.Property(e => e.PublishDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Article)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_Article_Category");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(100);
+
+                entity.HasOne(d => d.Article)
+                    .WithMany(p => p.Comment)
+                    .HasForeignKey(d => d.ArticleId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK_Comment_Article");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ AngularBlogCore.API/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AngularBlogCore.API.Models
+{
+    public partial class Comment
+    {
+        public int Id { get; set; }
+        public int? ArticleId { get; set; }
+        public string Name { get; set; }
+        public string ContentMain { get; set; }
+
+        public virtual Article Article { get; set; }
+    }
+}
